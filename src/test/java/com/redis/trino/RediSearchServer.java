922a6067e7688cd9af990c23ae666430deed6ddc
Julien Ruaux <jruaux@users.noreply.github.com>
2023-09-01 10:41:04 -0700

    test: Using redis-stack 6.2.6 instead of latest

--- src/test/java/com/redis/trino/RediSearchServer.java
+++ src/test/java/com/redis/trino/RediSearchServer.java
@@ -14,37 +14,41 @@ import io.lettuce.core.RedisURI;
 
 public class RediSearchServer implements Closeable {
 
-	private static final DockerImageName DOCKER_IMAGE_NAME = RedisStackContainer.DEFAULT_IMAGE_NAME
-			.withTag(RedisStackContainer.DEFAULT_TAG);
-	private final RedisStackContainer container = new RedisStackContainer(DOCKER_IMAGE_NAME).withEnv("REDISEARCH_ARGS",
-			"MAXAGGREGATERESULTS -1");
-	private final AbstractRedisClient client;
-	private final StatefulRedisModulesConnection<String, String> connection;
+    private static final String TAG = "6.2.6-v9";
 
-	public RediSearchServer() {
-		this.container.start();
-		this.client = ClientBuilder.create(RedisURI.create(container.getRedisURI())).cluster(container.isCluster())
-				.build();
-		this.connection = RedisModulesUtils.connection(client);
-	}
+    private static final DockerImageName DOCKER_IMAGE_NAME = RedisStackContainer.DEFAULT_IMAGE_NAME.withTag(TAG);
 
-	public String getRedisURI() {
-		return container.getRedisURI();
-	}
+    private final RedisStackContainer container = new RedisStackContainer(DOCKER_IMAGE_NAME).withEnv("REDISEARCH_ARGS",
+            "MAXAGGREGATERESULTS -1");
 
-	public AbstractRedisClient getClient() {
-		return client;
-	}
+    private final AbstractRedisClient client;
 
-	public StatefulRedisModulesConnection<String, String> getConnection() {
-		return connection;
-	}
+    private final StatefulRedisModulesConnection<String, String> connection;
 
-	@Override
-	public void close() {
-		connection.close();
-		client.shutdown();
-		client.getResources().shutdown();
-		container.close();
-	}
+    public RediSearchServer() {
+        this.container.start();
+        this.client = ClientBuilder.create(RedisURI.create(container.getRedisURI())).cluster(container.isCluster()).build();
+        this.connection = RedisModulesUtils.connection(client);
+    }
+
+    public String getRedisURI() {
+        return container.getRedisURI();
+    }
+
+    public AbstractRedisClient getClient() {
+        return client;
+    }
+
+    public StatefulRedisModulesConnection<String, String> getConnection() {
+        return connection;
+    }
+
+    @Override
+    public void close() {
+        connection.close();
+        client.shutdown();
+        client.getResources().shutdown();
+        container.close();
+    }
+
 }
